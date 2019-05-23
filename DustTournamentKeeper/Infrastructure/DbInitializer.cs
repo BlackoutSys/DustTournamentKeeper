b09cba761b04cda82737c88d83c13f3e73de852d
RESPONSEGROUP\SPietrzak <spietrzak@promotionalypayments.com>
2019-05-23 15:09:42 +0200

    DbInitializer initial settings

--- DustTournamentKeeper/Infrastructure/DbInitializer.cs
+++ DustTournamentKeeper/Infrastructure/DbInitializer.cs
@@ -69,6 +69,167 @@ namespace DustTournamentKeeper.Infrastructure
                         }
                     }
                 }
+
+                // Dust specific settings
+                var repository = serviceScope.ServiceProvider.GetRequiredService<ITournamentRepository>();
+
+                // Game
+                var games = repository.Games.ToList();
+                if (games.FirstOrDefault(g => g.Name == "Dust1947") == null)
+                {
+                    var game = new Game
+                    {
+                        Name = "Dust1947"
+                    };
+                    repository.Add(game);
+                }
+
+                // Blocks
+                var dustGame = repository.Games.FirstOrDefault(g => g.Name == "Dust1947");
+                var blocks = repository.Blocks.ToList();
+                if (blocks.FirstOrDefault(b => b.Name == "Axis") == null)
+                {
+                    var block = new Block
+                    {
+                        Name = "Axis",
+                        GameId = dustGame.Id
+                    };
+                    repository.Add(block);
+                }
+
+                if (blocks.FirstOrDefault(b => b.Name == "Allies") == null)
+                {
+                    var block = new Block
+                    {
+                        Name = "Allies",
+                        GameId = dustGame.Id
+                    };
+                    repository.Add(block);
+                }
+
+                if (blocks.FirstOrDefault(b => b.Name == "SSU") == null)
+                {
+                    var block = new Block
+                    {
+                        Name = "SSU",
+                        GameId = dustGame.Id
+                    };
+                    repository.Add(block);
+                }
+
+                if (blocks.FirstOrDefault(b => b.Name == "Mercenaries") == null)
+                {
+                    var block = new Block
+                    {
+                        Name = "Mercenaries",
+                        GameId = dustGame.Id
+                    };
+                    repository.Add(block);
+                }
+
+                if (blocks.FirstOrDefault(b => b.Name == "IJN") == null)
+                {
+                    var block = new Block
+                    {
+                        Name = "IJN",
+                        GameId = dustGame.Id
+                    };
+                    repository.Add(block);
+                }
+
+                // Factions
+                var factions = repository.Factions.ToList();
+
+                var axisBlock = repository.Blocks.FirstOrDefault(b => b.Name == "Axis");
+                if (factions.FirstOrDefault(f => f.Name == "Luftwaffe") == null)
+                {
+                    var faction = new Faction
+                    {
+                        Name = "Luftwaffe",
+                        GameId = dustGame.Id,
+                        BlockId = axisBlock.Id
+                    };
+                    repository.Add(faction);
+                }
+
+                if (factions.FirstOrDefault(f => f.Name == "NDAK") == null)
+                {
+                    var faction = new Faction
+                    {
+                        Name = "NDAK",
+                        GameId = dustGame.Id,
+                        BlockId = axisBlock.Id
+                    };
+                    repository.Add(faction);
+                }
+
+                if (factions.FirstOrDefault(f => f.Name == "Blutkreuz") == null)
+                {
+                    var faction = new Faction
+                    {
+                        Name = "Blutkreuz",
+                        GameId = dustGame.Id,
+                        BlockId = axisBlock.Id
+                    };
+                    repository.Add(faction);
+                }
+
+                var alliesBlock = repository.Blocks.FirstOrDefault(b => b.Name == "Allies");
+                if (factions.FirstOrDefault(f => f.Name == "Marines") == null)
+                {
+                    var faction = new Faction
+                    {
+                        Name = "Marines",
+                        GameId = dustGame.Id,
+                        BlockId = alliesBlock.Id
+                    };
+                    repository.Add(faction);
+                }
+
+                if (factions.FirstOrDefault(f => f.Name == "Desert Scorpions") == null)
+                {
+                    var faction = new Faction
+                    {
+                        Name = "Desert Scorpions",
+                        GameId = dustGame.Id,
+                        BlockId = alliesBlock.Id
+                    };
+                    repository.Add(faction);
+                }
+
+                var ssuBlock = repository.Blocks.FirstOrDefault(b => b.Name == "SSU");
+                if (factions.FirstOrDefault(f => f.Name == "Spetsnaz") == null)
+                {
+                    var faction = new Faction
+                    {
+                        Name = "Spetsnaz",
+                        GameId = dustGame.Id,
+                        BlockId = ssuBlock.Id
+                    };
+                    repository.Add(faction);
+                }
+
+                if (factions.FirstOrDefault(f => f.Name == "Red Guard") == null)
+                {
+                    var faction = new Faction
+                    {
+                        Name = "Red Guard",
+                        GameId = dustGame.Id,
+                        BlockId = ssuBlock.Id
+                    };
+                    repository.Add(faction);
+                }
+
+                if (factions.FirstOrDefault(f => f.Name == "PLA") == null)
+                {
+                    var faction = new Faction
+                    {
+                        Name = "PLA",
+                        GameId = dustGame.Id,
+                        BlockId = ssuBlock.Id
+                    };
+                    repository.Add(faction);
+                }
             }
         }
     }
